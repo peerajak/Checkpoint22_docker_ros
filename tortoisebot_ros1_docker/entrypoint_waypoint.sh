c7ca50cef2285c2961ebb7c9d1dbb78100e8db20
peerajak <peerajak@gmail.com>
2024-11-27 04:39:53 +0000

    ros1 sim, and ros2 sim works on the constructs machine

--- tortoisebot_ros1_docker/entrypoint_waypoint.sh
+++ tortoisebot_ros1_docker/entrypoint_waypoint.sh
@@ -2,11 +2,9 @@
 
 source /tortoisebot_ws/devel/setup.bash
 echo "$(date +'[%Y-%m-%d %T]') Starting rosbridge server..."
-roslaunch course_web_dev_ros web.launch &
-sleep 3
+roslaunch course_web_dev_ros --wait web.launch &
 echo "$(date +'[%Y-%m-%d %T]') Starting tf2_web server..." 
-roslaunch course_web_dev_ros tf2_web.launch  &
-sleep 3
+roslaunch course_web_dev_ros --wait tf2_web.launch  &
 echo "$(date +'[%Y-%m-%d %T]') Starting action server..." 
 rosrun course_web_dev_ros tortoisebot_action_server.py
 
