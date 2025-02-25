af84bec664ec5d22a776a14200dca910580630cf
peerajak <peerajak@gmail.com>
2025-02-25 19:23:21 +0700

    home ryzen

--- tortoisebot_ros1_docker/entrypoint_waypoint.sh
+++ tortoisebot_ros1_docker/entrypoint_waypoint.sh
@@ -2,7 +2,7 @@
 
 source /tortoisebot_ws/devel/setup.bash
 echo "$(date +'[%Y-%m-%d %T]') Starting rosbridge server..."
-roslaunch course_web_dev_ros --wait web.launch &
+roslaunch course_web_dev_ros --wait web2.launch &
 echo "$(date +'[%Y-%m-%d %T]') Starting tf2_web server..." 
 roslaunch course_web_dev_ros --wait tf2_web.launch  &
 echo "$(date +'[%Y-%m-%d %T]') Starting action server..." 
